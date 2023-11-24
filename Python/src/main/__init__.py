68ea0e89c6e670bd8567816d6926cd35a55ca2a3
Richone01 <leonrichter01@gmail.com>
2023-11-24 21:37:24 +0100

    Working on initial setup

--- Python/src/main/__init__.py
+++ Python/src/main/__init__.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+import os
+import psycopg2
 
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('building_id')
 
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='RoomManager',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
+
+
 class Building:
 
     def get(self):
         args = parser.parse_args()
+        conn = get_db_connection()
 
 
 api.add_resource(Building, '/building')
