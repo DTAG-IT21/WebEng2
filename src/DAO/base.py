09ff251743b6c0483a88ad373edefed46a141f44
Richone01 <leonrichter01@gmail.com>
2024-01-12 21:12:09 +0100

    Changed environment variables to fit kustomize schema

--- src/DAO/base.py
+++ src/DAO/base.py
@@ -4,11 +4,11 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-user = os.getenv("DB_USERNAME")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-port = os.getenv("DB_PORT")
-database = os.getenv("DB_NAME")
+user = os.getenv("POSTGRES_ASSETS_USER")
+password = os.getenv("POSTGRES_ASSETS_PASSWORD")
+host = os.getenv("POSTGRES_ASSETS_HOST")
+port = os.getenv("POSTGRES_ASSETS_PORT")
+database = os.getenv("POSTGRES_ASSETS_DBNAME")
 
 engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
 Session = sessionmaker(bind=engine)
