cdefd9e738cb4d0646fb53bf239f1c16594121ea
Richone01 <leonrichter01@gmail.com>
2024-01-12 15:34:38 +0100

    Implemented SQLAlchemy for storey_by_id.py

--- src/assets/storey_by_id.py
+++ src/assets/storey_by_id.py
@@ -1,61 +1,83 @@
+import datetime
+
 import src.main.database as database
 import src.main.response_generator as response_generator
+
+import src.main.response_generator as response_generator
+from src.DAO.building_dao import BuildingDAO
+from src.DAO.room_dao import RoomDAO
+from src.DAO.storey_dao import StoreyDAO
+from src.DAO.base import Session
+from sqlalchemy import and_
+
+session = Session()
 
 
 def handle_get(storey_id):
 
-    storey = database.select("id, name, building_id", "storeys", f"id = '{storey_id}'")
+    storey = session.query(StoreyDAO).get(storey_id)
+
     if not storey:
         message = "storey not found",
         more_info = "No storey with the given id found"
         return response_generator.error_response(message, more_info, status=404)
 
-    response_body = {"id": storey[0][0], "name": storey[0][1], "building_id": storey[0][2]}
+    response_body = {"id": str(storey.id), "name": storey.name, "building_id": str(storey.building_id)}
     return response_generator.response_body(response_body)
 
 
 def handle_put(storey_id, name, building_id, deleted_at):
     # Check if building not deleted
-    building = database.select("id, deleted_at", "buildings", f"id = '{building_id}'")
-    if not (building and building[0][1] is None):
+    building = session.query(BuildingDAO).get(building_id)
+    if not building:
         message = "building not found"
         more_info = "The requested building does not exist. Maybe it was deleted?"
         return response_generator.error_response(message, more_info, status=404)
 
     # Check if storey name is already in use
-    existing_storey = database.select("id, deleted_at", "storeys", f"name='{name}' and building_id = '{building_id}'")
-    if existing_storey and (existing_storey[0][0] != storey_id or existing_storey[0][1] is None):
+    existing_storey = session.query(StoreyDAO) \
+    .filter(and_(StoreyDAO.name == name,
+                 StoreyDAO.building_id == building_id)) \
+    .first()
+
+    if existing_storey and (str(existing_storey.id) != str(storey_id) or existing_storey.deleted_at is None):
         message = "storey name already used"
         more_info = "The given storey name is already in use in the specified building"
         return response_generator.error_response(message, more_info, status=400)
 
-    storey = database.select("*", "storeys", f"id = '{storey_id}' and building_id = '{building_id}'")
-    # Check if storey is deleted
-    if storey and storey[0][3] is not None:
-        # Check if storey shall be restored
-        if deleted_at is None:
-            database.update("storeys",
-                            f"id='{storey_id}', name='{name}', building_id='{building_id}', deleted_at=null",
-                            f"id='{storey_id}' and building_id='{building_id}'")
-            response_body = {
-                "id": str(storey_id),
-                "name": name,
-                "building_id": str(building_id)
-            }
-            return response_generator.response_body(response_body)
-        else:
-            message = "storey not found"
-            more_info = "storey not found or deleted. If you want to restore the storey, pass deleted_at: null."
-            return response_generator.error_response(message, more_info, status=404)
-    elif storey:
-        if deleted_at is None:
+    updated_storey = session.query(StoreyDAO).get(storey_id)
+    # Check if storey exists
+    if updated_storey:
+        # Check if storey was deleted
+        if updated_storey.deleted_at is not None:
+            # Check if storey shall be restored
+            if deleted_at is None:
+                updated_storey.name = name
+                updated_storey.building_id = building_id
+                updated_storey.deleted_at = None
+                session.commit()
+
+                response_body = {
+                    "id": str(storey_id),
+                    "name": name,
+                    "building_id": str(building_id)
+                }
+                return response_generator.response_body(response_body)
+            else:
+                message = "storey not found"
+                more_info = "storey not found or deleted. If you want to restore the storey, pass deleted_at: null."
+                return response_generator.error_response(message, more_info, status=404)
+        # Check if the storey shall be restored (although not deleted)
+        elif deleted_at is None:
             message = "Bad Request"
             more_info = "storey cannot be restored, as it is not deleted"
             return response_generator.error_response(message, more_info, status=400)
+        # Update Storey
         else:
-            database.update("storeys",
-                            f"id='{storey_id}', name='{name}', building_id='{building_id}', deleted_at=null",
-                            f"id='{storey_id}' and building_id='{building_id}'")
+            updated_storey.name = name
+            updated_storey.building_id = building_id
+            session.commit()
+
             response_body = {
                 "id": str(storey_id),
                 "name": name,
@@ -69,13 +91,17 @@ def handle_put(storey_id, name, building_id, deleted_at):
 
 
 def handle_delete(storey_id):
-    storey = database.select("*", "storeys", f"id = '{storey_id}' and deleted_at is null")
+    storey = session.query(StoreyDAO) \
+        .filter(and_(StoreyDAO.id == storey_id,
+                     StoreyDAO.deleted_at.is_(None))) \
+        .first()
     if storey:
-        rooms = database.select("*", "rooms", f"storey_id = '{storey_id}'")
+        rooms = session.query(RoomDAO) \
+            .filter(RoomDAO.storey_id == storey_id) \
+            .all()
         if not rooms:
-            database.update("storeys",
-                            f"id = '{storey[0][0]}', name = {storey[0][1]}, building_id = '{storey[0][2]}', deleted_at = CURRENT_TIMESTAMP",
-                            f"id = '{storey_id}'")
+            storey.deleted_at = datetime.datetime.now()
+            session.commit()
             return response_generator.no_content()
         else:
             message = "Storey cannot be deleted"
